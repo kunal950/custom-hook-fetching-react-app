e4a2ceec5f925e295cb111707892125c3f01df45
Kunal <kunalagarwal9204@gmail.com>
2024-05-25 15:24:07 +0530

    added error page and card page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import Error from "./components/Error";
+import useFetch from "./hooks/useFetch";
+import Loading from "./components/Loading";
+import Cards from "./components/Cards";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { loading, error, data, photos } = useFetch();
 
   return (
     <>
-      <div>
-        <div>hello</div>
+      <div className="min-h-screen bg-gray-800 flex justify-center items-center">
+        {error && !loading && <div><Error/></div>}
+        {!error && loading && <div><Loading/></div>}
+        {!error && !loading && <div><Cards photos={photos} data={data}/></div>}
+        {(loading && !error)?(<Loading />):(<Cards photos={photos} data={data}/>)}
       </div>
     </>
   );

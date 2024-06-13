60a49f2df8d83d63575bc574b25bae801272eedb
Kunal Agarwal <kunalagarwal9204@gmail.com>
2024-06-13 18:14:15 +0530

    solving mapping error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({title,url}) => {
+const Card = ({ title, url }) => {
   return (
     <div className="w-[300px] border-2 border-white p-[3px] mb-10 mx-1 max-h-fit">
       <div className="overflow-hidden">
@@ -9,11 +9,11 @@ const Card = ({title,url}) => {
         </div>
 
         <div className=" text-center p-4 text-white">
-          <p className="font-bold text-lg leading-6">{title}</p>          
+          <p className="font-bold text-lg leading-6">{title}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,47 +1,29 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 const useFetch = () => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [data, setData] = useState([]);
-    const[photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [photos, setPhotos] = useState([]);
 
-    const getPhotos = async () => {
-        setLoading(true);
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=100');
-            const data = await response.json();
-            setPhotos(data);
-        }
-        catch (error) {
-            setError(true);
-        }
-        finally {
-            setLoading(false);
-        }
+  const getPhotos = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/photos?_limit=100"
+      );
+      const data = await response.json();
+      setPhotos(data);
+    } catch (error) {
+      setError(true);
     }
+    setLoading(false);
+  };
 
-    const getpost = async () => {
-        setLoading(true);
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await response.json();
-            setData(data);
-        }
-        catch (error) {
-            setError(true);
-        }
-        finally {
-            setLoading(false);
-        }
-    }
+  useEffect(() => {
+    getPhotos();
+  }, []);
 
-    useEffect(() => {
-        getPhotos();
-        getpost();        
-    }, []);
+  return { loading, error, photos };
+};
 
-    return {loading, error, data, photos};
-}
-
-export default useFetch
+export default useFetch;
